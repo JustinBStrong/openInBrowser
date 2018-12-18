7e9e77ccb8d1e81a602dc71ce96b1e8791d1c0e9
JustinBStrong <jbstrong@cpp.edu>
2018-12-18 04:50:16 -0800

    Added Save before open feature and preference to turn it off

--- main.js
+++ main.js
@@ -5,11 +5,17 @@
 define(function (require, exports, module) {
     "use strict";
     var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus"),
+        Menus = brackets.getModule("command/Menus"),
         NativeApp = brackets.getModule("utils/NativeApp"),
-        ProjectManager = brackets.getModule("project/ProjectManager");
-    
+        ProjectManager = brackets.getModule("project/ProjectManager"),
+        Commands = brackets.getModule("command/Commands"),
+        PreferencesManager = brackets.getModule("preferences/PreferencesManager"),
+        prefs = PreferencesManager.getExtensionPrefs("OpenInBrowser");
+
     function handleOpenInBrowser() {
+        if (prefs.get("SaveFileBeforeOpening")) {
+            CommandManager.execute("file.save");
+        }
         var doc = ProjectManager.getSelectedItem();
         if (doc) {
             //console.log(doc._path);
@@ -22,9 +28,16 @@ define(function (require, exports, module) {
             console.log("error 1");
         }
     }
-    
-    var openInBrowser = "strong.justin.openinbrowser";  
+
+    var openInBrowser = "strong.justin.openinbrowser";
     CommandManager.register("Open in Browser", openInBrowser, handleOpenInBrowser);
     var workingsetMenu = Menus.getContextMenu(Menus.ContextMenuIds.WORKING_SET_CONTEXT_MENU);
     workingsetMenu.addMenuItem(openInBrowser, "Ctrl-2");
+
+    //This sets the default preference for saving files before opening them. If this preference is changed to false files will no longer be saved before opening them.
+    prefs.definePreference("SaveFileBeforeOpening", "boolean", true);
+    if (prefs.get("SaveFileBeforeOpening")) {
+        prefs.set("SaveFileBeforeOpening", true)
+    }
+    prefs.save();
 });
